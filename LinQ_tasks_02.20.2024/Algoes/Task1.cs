86794899361c53a6726e421449cbee031d16e535
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-02-20 16:45:18 +0500

    Added GetBuxgalters

--- LinQ_tasks_02.20.2024/Algoes/Task1.cs
+++ LinQ_tasks_02.20.2024/Algoes/Task1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinQ_tasks_02._20._2024.Models.Bugal_ProgLang;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,20 @@ namespace LinQ_tasks_02._20._2024.Algoes
 {
     public class Task1
     {
+        public static List<Buxgalter> GetBuxgalters()
+        {
+            List<Buxgalter> buxgalters = new List<Buxgalter>
+            {
+                new Buxgalter{Id = 1,BName= "Alsh",ProgrammingLanguageId = 1},
+                new Buxgalter{Id = 2,BName= "Vali",ProgrammingLanguageId = 1},
+                new Buxgalter{Id = 3,BName= "G'oppor",ProgrammingLanguageId = 1},
+                new Buxgalter{Id = 4,BName= "Umid",ProgrammingLanguageId = 2},
+                new Buxgalter{Id = 5,BName= "Rocky",ProgrammingLanguageId = 2},
+                new Buxgalter{Id = 6,BName= "Tommy",ProgrammingLanguageId = 3},
+                new Buxgalter{Id = 6,BName= "Jon",ProgrammingLanguageId = 3}
+
+            };
+            return buxgalters;
+        }
     }
 }
--- LinQ_tasks_02.20.2024/Program.cs
+++ LinQ_tasks_02.20.2024/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace LinQ_tasks_02._20._2024
+﻿using LinQ_tasks_02._20._2024.Algoes;
+using LinQ_tasks_02._20._2024.Models.Bugal_ProgLang;
+
+namespace LinQ_tasks_02._20._2024
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            foreach (Buxgalter emp in Task1.GetBuxgalters())
+            {
+                Console.WriteLine($"{emp.Id} {emp.BName} {emp.ProgrammingLanguageId}");
+            }
         }
     }
 }
