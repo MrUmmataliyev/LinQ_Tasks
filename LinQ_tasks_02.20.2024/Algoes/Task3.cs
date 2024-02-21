18c6a0785c099919c4f71d6c56144820aa73ccae
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-02-21 08:37:16 +0500

    Task3 finished

---
+++ LinQ_tasks_02.20.2024/Algoes/Task3.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace LinQ_tasks_02._20._2024.Algoes
+{
+    public class Task3
+    {
+        public static void GetBuxgGroupByProgramLang()
+        {
+         var langs = Task1.GetProgrammingLanguages();
+         var buxg = Task1.GetBuxgalters();
+         var res = langs
+                .GroupJoin(buxg, langs => langs.Id, buxg => buxg.ProgrammingLanguageId, 
+                (langs, buxg) => new { langs, buxg });
+         foreach (var bux in res)
+         {
+                Console.WriteLine(bux.langs.Name+" "+JsonConvert.SerializeObject(bux.buxg, Formatting.Indented));
+         }
+        
+        }
+    }
+}
--- LinQ_tasks_02.20.2024/Program.cs
+++ LinQ_tasks_02.20.2024/Program.cs
@@ -7,7 +7,18 @@ namespace LinQ_tasks_02._20._2024
     {
         static void Main(string[] args)
         {
+            Task1.KnowCoding();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
             Task2.GetCSharpBuxgalters();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Task3.GetBuxgGroupByProgramLang();
+
         }
     }
 }
